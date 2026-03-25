4239f1dc6c9ff26806929d83105f9e679de9d1d6
Nick mwathiki <mwathikinicholas20@gmail.com>
2026-03-25 04:53:22 +0000

    Move Firebase config to env variables

--- src/firebaseClient.js
+++ src/firebaseClient.js
@@ -1,21 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCG7wj0SjLSIm8C0eLDGKZV9mdyDuOdXwM",
-  authDomain: "workshop-genai-477501.firebaseapp.com",
-  databaseURL: "https://workshop-genai-477501-default-rtdb.firebaseio.com",
-  projectId: "workshop-genai-477501",
-  storageBucket: "workshop-genai-477501.firebasestorage.app",
-  messagingSenderId: "471218709027",
-  appId: "1:471218709027:web:02176809f94745617a0796",
-  measurementId: "G-P6WQYJJRFP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
